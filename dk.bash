58bbdb7e870606a55b1a323babca64bde95736c2
Peter Carrero <peter.carrero@gmail.com>
2020-09-01 09:08:05 -0500

    docker: Add dk helper command

--- dk.bash
+++ dk.bash
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-docker run --rm -ti -v $PWD:/app -w /app hidgweb/node-chromium $@
+docker volume create e2e-testing-fpx__node_modules
+docker run --rm -ti -v $PWD:/app:delegated -v e2e-testing-fpx__node_modules:/app/node_modules:delegated -w /app hidgweb/node-chromium $@
 
